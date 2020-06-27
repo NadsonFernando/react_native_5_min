6e6e5496a2aad8c1daf62dcc356e377428245eac
Nádson <nadsonfernando1@gmail.com>
2020-06-27 12:45:18 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Loader from './src/components/Loader';
+import {Container} from './styles';
+
+export default function App() {
+  return (
+    <Container>
+      <Loader />
+    </Container>
+  );
+}
